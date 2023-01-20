8d0d694cd93a48b15c270a08d24decae3db60938
Stefan Espant <info@espant.com>
2023-01-20 09:08:58 +0100

    v2.1
    dialog aangemaakt. nieuwe variabelen aangemaakt in de index.js

---
+++ script/index.js
@@ -0,0 +1,32 @@
+const productHero = document.querySelector('product-hero')
+
+productHero.addEventListener('mousemove', () => {
+
+} )
+
+
+// Settings for the media dialog
+const showImage = document.getElementById('showImages')
+const dialogImage = document.getElementById('dialogImages')
+
+const showVideo = document.querySelector('.showVideo')
+const dialogVideo = document.querySelector('#dialogVideo')
+
+const show360 = document.querySelector('.show360')
+const dialog360 = document.querySelector('#dialog360')
+
+const showPlan = document.querySelector('.showPlan')
+const dialogPlan = document.querySelector('#dialogPlan')
+
+// dialog for images
+showImage.addEventListener('click', () => {
+    dialogImage.showModal()
+})
+
+// dialog for video's
+
+
+// dialog for 360 images
+
+
+// dialog for ground plan images
