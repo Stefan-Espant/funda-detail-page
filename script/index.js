d37dcf2efa396ab42f9a0fe51fb5fd7b224400de
Stefan Espant <info@espant.com>
2023-01-24 14:05:06 +0100

    v3.2
    html validatie fix

--- script/index.js
+++ script/index.js
@@ -1,41 +1,41 @@
-// dialog for video's
-const openDialogVideo = document.querySelector(".open-dialog-video");
-const dialogVideo = document.querySelector("dialog-video");
-const closeDialogVideo = document.querySelector(".close-button");
+// // dialog for video's
+// const openDialogVideo = document.querySelector(".open-dialog-video");
+// const dialogVideo = document.querySelector("dialog-video");
+// const closeDialogVideo = document.querySelector(".close-button");
 
-openDialogVideo.addEventListener("click", function() {
-  dialogVideo.showModal();
-});
+// openDialogVideo.addEventListener("click", function() {
+//   dialogVideo.showModal();
+// });
 
-closeDialogVideo.addEventListener("click", function() {
-  dialog.close();
-});
+// closeDialogVideo.addEventListener("click", function() {
+//   dialog.close();
+// });
 
-// dialog for 360 images
-const openDialog360Img = document.querySelector(".open-dialog-360");
-const dialog360 = document.querySelector("dialog-360");
-const closeDialog360Img = document.querySelector(".close-button");
+// // dialog for 360 images
+// const openDialog360Img = document.querySelector(".open-dialog-360");
+// const dialog360 = document.querySelector("dialog-360");
+// const closeDialog360Img = document.querySelector(".close-button");
 
-openDialog360Img.addEventListener("click", function() {
-  dialog.showModal();
-});
+// openDialog360Img.addEventListener("click", function() {
+//   dialog.showModal();
+// });
 
-closeDialog360Img.addEventListener("click", function() {
-  dialog.close();
-});
+// closeDialog360Img.addEventListener("click", function() {
+//   dialog.close();
+// });
 
-// dialog for ground plan images
-const openDialogGroundPlan = document.querySelector(".open-dialog-ground-plan");
-const dialogGroundPlan = document.querySelector("dialog-ground-plan");
-const closeDialogGroundPlan = document.querySelector(".close-button");
+// // dialog for ground plan images
+// const openDialogGroundPlan = document.querySelector(".open-dialog-ground-plan");
+// const dialogGroundPlan = document.querySelector("dialog-ground-plan");
+// const closeDialogGroundPlan = document.querySelector(".close-button");
 
-openDialogVideo.addEventListener("click", function() {
-  dialog.showModal();
-});
+// openDialogVideo.addEventListener("click", function() {
+//   dialog.showModal();
+// });
 
-closeDialogVideo.addEventListener("click", function() {
-  dialog.close();
-});
+// closeDialogVideo.addEventListener("click", function() {
+//   dialog.close();
+// });
 
 
 // Settings for the image carousel with thumbnails
