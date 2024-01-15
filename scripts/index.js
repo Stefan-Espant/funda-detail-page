c87d36f413ff92c975ed95bc24844afb9a06ffed
Daan645 <daan@videotechniek.nl>
2024-01-15 15:44:55 +0100

    slider
    made the image slider for mobile view

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,8 @@
 let currentSlide = 1;
 
 function changeSlide(direction) {
+    const nextButton = document.querySelector('.next');
+    const prevButton = document.querySelector('.prev');
     const slider = document.querySelector('.slider');
     const totalSlides = document.querySelectorAll('.slide').length;
 
@@ -9,9 +11,17 @@ function changeSlide(direction) {
     } else if (direction === 1 && currentSlide < totalSlides) {
         currentSlide++;
     } else if (direction === 1 && currentSlide === totalSlides) {
-        // Als de huidige dia de laatste is en de gebruiker op "Next" drukt,
-        // ga terug naar de eerste dia.
         currentSlide = 1;
+    }
+
+    // Als je op de derde dia bent, verander de afbeelding van de rechterpijl
+    if (currentSlide === 2, 3) {
+        nextButton.innerHTML = '<img src="assets/arrowRightBlack.png" alt="Right Arrow">';
+        // prevButton.innerHTML = '<img src="assets/arrowLeftBlack.png" alt="Right Arrow">';
+    } if (currentSlide === 1) {
+        // Als je niet op de derde dia bent, gebruik de standaard afbeelding van de rechterpijl
+        nextButton.innerHTML = '<img src="assets/arrowWhiteRight.png" alt="Right Arrow">';
+        // prevButton.innerHTML = '<img src="assets/arrowLeftWhite.png" alt="Right Arrow">';
     }
 
     const translateValue = -100 * (currentSlide - 1);
